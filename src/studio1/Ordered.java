be54b951d98d7080991217595460b551c120d1e1
lukebekemeyer <luke.bekemeyer@wustl.edu>
2019-08-29 13:34:49 -0500

    Done

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -13,6 +13,10 @@ public class Ordered {
 		int y = ap.nextInt("Value for y?");
 		int z = ap.nextInt("Value for z?");
 
+	boolean isOrdered = (x < y && y < z) || (z < y && y < x);
+		System.out.println(isOrdered);
+
+
 	}
 
 }
